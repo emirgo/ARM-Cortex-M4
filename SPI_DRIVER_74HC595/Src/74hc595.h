edd7cc70a49679c849171876cac2e84dcf3874a2
Emir <40762681+emirgo@users.noreply.github.com>
2020-04-09 19:32:15 +0200

    Servo

--- SPI_DRIVER_74HC595/Src/74hc595.h
+++ SPI_DRIVER_74HC595/Src/74hc595.h
@@ -6,14 +6,14 @@
 // 7 Segment display data. Only numeric.
 uint8_t SEG_num_data[10] = 
 {
-	0xc0,
-	0xf9,
-	0x5b,
-	0xa4,
+	0xC0,
+	0xCF,
+	0xA4,
+	0xB0,
 	0x99,
 	0x92,
 	0x82,
-	0xf8,
+	0xF8,
 	0x80,
 	0x90
 };
@@ -32,11 +32,11 @@ void SEG_shift_out(uint8_t *data, uint16_t size, SPI_HandleTypeDef *hspi)
 	HAL_GPIO_WritePin(GPIOD, LATCH_1_Pin, GPIO_PIN_SET);
 }
 
-void SEG_count_example(SPI_HandleTypeDef *hspi)
+void SEG_count_example(SPI_HandleTypeDef *hspi, uint32_t delay)
 {
 	for(uint8_t index = 0; index < 10; index++)
 	{
 		SEG_shift_out(SEG_num_data + index, 1, hspi);
-		HAL_Delay(1000);
+		HAL_Delay(delay);
 	}
 }
